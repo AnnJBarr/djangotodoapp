0a9c0e479174a55552d828b125e7f17a57a9633c
AnnJBarr <annbarr@hotmail.com>
2020-02-26 16:06:36 +0000

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    task_text = models.CharField(max_length=1000)
+    created_date = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField()
+    complete = models.BooleanField()
